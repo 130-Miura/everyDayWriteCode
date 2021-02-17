5e027f641389e0aa5592249d0655478818268b65
130-Miura <gfc1300@docomo.ne.jp>
2021-02-17 23:06:34 +0900

    changed the content

--- bit_flag.py
+++ bit_flag.py
@@ -1,42 +1,52 @@
-# フラグ管理
+# 2進数を利用したフラグ管理
+
+# フラグが立っているかを確認する
+# @target フラグが格納されている変数
+# @flag 確認するフラグ
+# @return フラグが立っていればTrue、降りていればFalseを返す
+def check_flag(target, flag):
+    return True if (target & flag) > 0 else False
+
+# フラグが立っていない場合、フラグを立てる
+# @return フラグが既に立っていればtargetの値をそのまま返し、立っていなければフラグを立てた値を返す
+def up_flag(target, flag):
+
+    # フラグが降りているか確認
+    if check_flag(target, flag) == False:
+
+        # フラグが降りている場合、フラグを立てる
+        target |= flag
+    return target
+
+# フラグが立っていない場合、フラグを降ろす
+def down_flag(target, flag):
+
+    # フラグが立っているか確認
+    if check_flag(target, flag) == True:
+
+        # フラグが立っている場合、フラグを降ろす
+        target -= target & flag
+    return target
+
+# 関数のテスト
+
+# フラグの定義
 FLAG_A = 1
-FLAG_B = 2
-FLAG_C = 4
-FLAG_D = 8
-FLAG_E = 16
-FLAG_F= 32
-FLAG_G = 64
-FLAG_H = 128
 
 # フラグの初期化
 man = 0
 
-# Bのフラグを立てる
-man |= FLAG_B
-# print(man)
+# フラグの確認
+assert check_flag(man, FLAG_A) == False
 
-# Eのフラグを立てる
-man |= FLAG_E
-# print(man)
+# FLAG_Aを立てる
+man = up_flag(man, FLAG_A)
 
-# Aのフラグが立っているかを確認する
-if man & FLAG_A:
-    print('Aのフラグが立っている')
-else:
-    print('Aのフラグは立っていない')
-# print('AOK') if man & FLAG_A else print('ANG')
+# フラグが立っているか確認
+assert check_flag(man, FLAG_A) == True
 
-# Eのフラグが立っているかを確認する
-if man & FLAG_E:
-    print('Aのフラグが立っている')
-else:
-    print('Aのフラグは立っていない')
+# FLAG_Aを降ろす
+man = down_flag(man, FLAG_A)
 
-# BとEのフラグが立っている状態
-
-# Bのフラグを下ろす
-man -= man & FLAG_B
-if man & FLAG_B:
-    print('Bのフラグが立っている')
-else:
-    print('Bのフラグが立っていない')
+# フラグが降りているか確認
+assert check_flag(man, FLAG_A) == False
