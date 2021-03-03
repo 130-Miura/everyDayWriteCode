29352bece84267d7c3a5dbe9f047e30810172a82
130-Miura <gfc1300@docomo.ne.jp>
2021-03-03 23:33:48 +0900

    change content

--- about_decorator.py
+++ about_decorator.py
@@ -77,3 +77,21 @@ text = 'test text'
 # n = k(m)
 # print(n)
 
+# デコレータは以下のように呼ばれることが多いらしい
+# m = k(m)
+
+# デコレータの使用例
+# class Coordinate(object):
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#     def __repr__(self):
+#         return "Coord: " + str(self.__dict__)
+# def add(a, b):
+#     return Coordinate(a.x + b.x, a.y + b.y)
+# def sub(a, b):
+#     return Coordinate(a.x - b.x, a.y - b.y)
+# one = Coordinate(100, 200)
+# two = Coordinate(300, 200)
+# print(add(one, two))
+
