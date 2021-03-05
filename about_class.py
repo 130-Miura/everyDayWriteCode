16753110334de3e9cbc7641ee8d7a5757e32d438
130-Miura <gfc1300@docomo.ne.jp>
2021-03-05 22:28:48 +0900

    add file

---
+++ about_class.py
@@ -0,0 +1,9 @@
+class MyClass:
+    count = 0                       # クラス変数を初期化
+
+    def __init__(self):
+        MyClass.count += 1          # クラス変数をカウントアップ
+
+a1 = MyClass()
+a2 = MyClass()
+print(MyClass.count)                 #=> 2
