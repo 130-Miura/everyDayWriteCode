32ee2476e83b171ef2be1e5907af078b24c26644
130-Miura <gfc1300@docomo.ne.jp>
2021-03-10 23:10:54 +0900

    add file

---
+++ about_mock.py
@@ -0,0 +1,9 @@
+from unittest import mock
+
+# mockは特定のオブジェクトの代理をしてユニットテストを円滑に進めるためのモジュール
+# m = mock.MagicMock(a=1, b=2)
+# print(m)
+# print(m.k.c)
+# m = mock.MagicMock(return_value=5)
+# print(m())
+# print(m(5, 7, [9, 2]))
