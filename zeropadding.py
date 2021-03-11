282867b7f17b48b511255d670c86e67b1e0efbf3
130-Miura <gfc1300@docomo.ne.jp>
2021-03-11 22:32:21 +0900

    add file

--- zeropadding.py
+++ zeropadding.py
@@ -1,11 +1,11 @@
 # 文字列を右寄せする
 # 文字列.rjust(全体文字数,埋める文字列)
-n = '101'
-rn = n.rjust(5, '0')
-print(rn)
+# n = '101'
+# rn = n.rjust(5, '0')
+# print(rn)
 
 # 文字列を左寄せする 
 # 文字列.ljust(全体文字数,埋める文字列)
-n = '102'
-ln = n.ljust(6, '0')
-print(ln)
+# n = '102'
+# ln = n.ljust(6, '0')
+# print(ln)
