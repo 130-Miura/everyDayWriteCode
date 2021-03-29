dca1e53d139fd5744291d6177b0adb99322f9f8b
130-Miura <gfc1300@docomo.ne.jp>
2021-03-29 23:08:53 +0900

    change content

--- about_builtin.py
+++ about_builtin.py
@@ -6,3 +6,15 @@
 
 # 組み込み関数の個数（int()など、コンストラクタ含む）
 # print(len(list(name for name in vars(__builtins__) if (name[0].islower() and name not in ('quit', 'exit', 'copyright', 'credits', 'license',)) or name=='__import__')))
+
+# all() 要素内の全てがTrueならTrueを返す
+# a = 0
+# l = [a < 8, -6 < a, 7, not False, True]
+# if all(l):
+#     print('リスト内の要素(式の結果)は全てTrue')
+
+# any() 要素内のいずれかがTrueならTrueを返す
+# a = 0
+# l = [a < 8, -6 < a, 7, not False, True, False]
+# if any(l):
+#     print('リスト内の要素(式の結果)のいずれかがTrue')
