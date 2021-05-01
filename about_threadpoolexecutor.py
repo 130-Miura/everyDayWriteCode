bc689279f28794388bb683f92b26185f51cb20ef
130-Miura <gfc1300@docomo.ne.jp>
2021-05-01 23:12:46 +0900

    change content

--- about_threadpoolexecutor.py
+++ about_threadpoolexecutor.py
@@ -39,19 +39,44 @@
 # print('うどんが100個茹で上がりました。')
 
 # 以下の文はfeature.cancel()でキャンセルできずにFalseとなる
-# 別の記事で、スレッドの実行中はキャンセルできないが、processpoolを使えばキャンセル可能って書いてた
+# feature変数に入っているのはExecutor.submit()で返されたFetureクラスのインスタンス
+# 別の記事で、スレッドの実行中はキャンセルできないが、processpoolを使えばキャンセル可能って書いていた
 # import time
 # from concurrent.futures import ThreadPoolExecutor
 
 # def foo():
 #     time.sleep(3)
+#     print('a')
+#     time.sleep(5)
+#     print('b')
 #     return 'fooo'
 
 # with ThreadPoolExecutor() as executor:
 #     feature = executor.submit(foo)
-#     print(feature.running()) # True
-#     print(feature.done()) # False
-#     print(feature.cancel()) # 処理のキャンセル
+#     print(type(feature))
+#     print(feature.running())
+#     print(feature.done())
+#     print(feature.cancel())
+#     print(feature.running())
+#     print(feature.done())
+#     time.sleep(4)
+#     print(feature.cancel())
+#     print(feature.running())
+#     print(feature.done())
+#     print(feature.cancelled())
 
-#     print(feature.running()) # False
-#     print(feature.done()) # True
+# Executorクラスはインスタンス化されていない基底クラス
+# ThreadPoolExecutorとProcessPoolExecutorの2つのサブクラスを持つ
+# ThreadPoolExecutor：スレッドプールを指定して非同期処理を行う
+# ProcessPoolExecutor：プロセスプールを指定して非同期処理を行う
+# これらのサブクラスはExecutorクラスで定義された3つのメソッドが利用可能
+# submit(fn, *args, **kwargs)
+# 呼び出し可能オブジェクトfnをfn(*args, **kwargs)として実行するようにスケジュールし、
+# Futureオブジェクト（呼び出し可能オブジェクトfnの非同期実行をカプセル化したもの）を返す
+# map(func, *iterables, timeout=None, chunksize=1)
+# *iterablesの要素をひとつずつfuncの引数として渡して実行する
+# shutdown(wait=True)
+# executorに対して、現在保留中のすべてのFutureが実行された後で、資源を解放するように伝える
+# waitがTrueの場合、すべての未完了のFutureが完了してExecutorに関連づいたリソースが解放されるまで、このメソッドはreturnしない
+# waitがFalseの場合、このメソッドはすぐにreturnし、すべての未完了のFutureが実行完了したときに、Executorに関連づいたリソースが解放される
+# すべての未完了のFutureが実行完了するまでpythonプログラム全体は終了しない
