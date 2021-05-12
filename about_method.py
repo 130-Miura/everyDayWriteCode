be42f9db1951406f915873c3d3fabe13ac71821b
130-Miura <gfc1300@docomo.ne.jp>
2021-05-12 22:23:16 +0900

    change content

--- about_method.py
+++ about_method.py
@@ -3,13 +3,44 @@ class Food(object):
         self.food_name = food_name
     def get_food_name(self):
         return self.food_name
+    # 静的メソッドの2つのメリット
+    # self引数が必要ない場合に利用するとself引数がある場合と比べて早い
+    # クラスオブジェクトの状態に依存しないことが一目で分かる
+    @staticmethod
+    def x_plus_y(x, y):
+        return x + y
 
-print(Food.get_food_name)
-print(Food('apple').get_food_name())
-f = Food('orange').get_food_name
-print(f())
+    a_dislike_food = 'パクチー'
+    # クラスメソッドはクラスにバインドされる
+    # __init__と異なるシグネチャを使ってオブジェクトをインスタンス化するファクトリメソッドが作成できる
+    @classmethod
+    def dislike_food(cls):
+        return cls.a_dislike_food
+
+    @classmethod
+    def make_factory(cls, obj):
+        return obj.x + obj.y
+
+class x_and_y:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+inst = x_and_y(2, 8)
+
+# print(Food.make_factory(inst))
+
+# print(Food.get_food_name)
+# print(Food('apple').get_food_name())
+# f = Food('orange').get_food_name
+# print(f())
 
 # バインドされているオブジェクトの確認
-print(f.__self__)
-print(f == f.__self__.get_food_name)
+# print(f.__self__)
+# print(f == f.__self__.get_food_name)
 
+# 静的メソッドはインスタンス化せず呼び出し可能
+# print(Food.x_plus_y(2, 5))
+
+# クラスメソッドも同様。メソッドの引数にオブジェクトを渡してインスタンス化可能（ファクトリメソッド）
+# print(Food.dislike_food(), 'が嫌いだ', sep='')
