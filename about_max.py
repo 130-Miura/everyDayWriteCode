73e35ced13d329ac6133fde6631b4cbec5db73c6
130-Miura <gfc1300@docomo.ne.jp>
2021-05-26 23:16:05 +0900

    change content

--- about_max.py
+++ about_max.py
@@ -1,9 +1,12 @@
 # 複数のリストの要素のうち、最大値を返す
 l = [1, 5, 9, 12, 3]
 ll = [3, 7, 2]
-print(max(*l, *ll))
+print("max:", max(*l, *ll))
+print("min:", min(*l, *ll))
 
 # 辞書のキー、または値の最大値を返す
 dd = {3: 9, 5: 1, 3: 123}
-print(max(dd))
-print(max(dd.values()))
+print("max.key:", max(dd))
+print("max.values:", max(dd.values()))
+print("max.key:", min(dd))
+print("max.values:", min(dd.values()))
